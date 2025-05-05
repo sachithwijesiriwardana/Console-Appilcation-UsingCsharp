aac935bbb07fb2621893fcd53c74bc45149f61ed
Sachith Wijeisriwardana <sachith55cha1w@gmail.com>
2025-05-05 23:19:15 +0530

    fundermentals of c# basic

--- HelloFromCsharp/Program.cs
+++ HelloFromCsharp/Program.cs
@@ -1,8 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, everyBody!");
+﻿double wage  = 190;
+int nextwage = 330;
 
-Console.WriteLine("Enter Your Name:");
+double wholeweight = wage* nextwage;
 
-String name = Console.ReadLine();
+Console.WriteLine(wholeweight);
 
-Console.WriteLine("Hello " +  name);
+
+wage += 3;
+Console.WriteLine(wage);
+
+if (wage > 5225) ;
+Console.WriteLine("Top Paid Emplloyee");
+
+int numberOfEmployee = 15;
+numberOfEmployee--;
+
+DateTime hireDate = new DateTime(2022,3,28,14,30,0);
+Console.WriteLine(hireDate);
+
+DateTime startDate = hireDate.AddDays(15);
+Console.WriteLine(startDate);
+
+int numberOfHoursWorkd = 165;
+
+double d = 1252251511.0;
+int x = (int)d;
+
+
+//Booelan Types wuth relational and logical operator 
+
+
+
+//int age = 23;
+//bool a = age == 23;
+//Console.WriteLine("i need a answer "+a);
+
+
+Console.WriteLine("Enter the age of the new candidate");
+int agee = int.Parse(Console.ReadLine());
+
+if (agee < 18)
+{
+    Console.WriteLine("HI hi all");
+}
+else if(agee > 65)
+{
+    Console.WriteLine("HI hi all wer rwe rr");
+    Console.WriteLine("HI hi all wer rwrwr hi hhi hi we rr");
+}
+else { 
+    Console.WriteLine("Greate ,You can start with the application");
+}
+   
