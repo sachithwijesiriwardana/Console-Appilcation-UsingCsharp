902129d8da35fa0e17d558a58ec49d0f7f7bd967
Sachith Wijeisriwardana <sachith55cha1w@gmail.com>
2025-05-06 00:45:28 +0530

    While Loops

--- HelloFromCsharp/Program.cs
+++ HelloFromCsharp/Program.cs
@@ -1,33 +1,33 @@
-﻿double wage  = 190;
-int nextwage = 330;
+﻿//double wage  = 190;
+//int nextwage = 330;
 
-double wholeweight = wage* nextwage;
+//double wholeweight = wage* nextwage;
 
-Console.WriteLine(wholeweight);
-
-
-wage += 3;
-Console.WriteLine(wage);
-
-if (wage > 5225) ;
-Console.WriteLine("Top Paid Emplloyee");
-
-int numberOfEmployee = 15;
-numberOfEmployee--;
-
-DateTime hireDate = new DateTime(2022,3,28,14,30,0);
-Console.WriteLine(hireDate);
-
-DateTime startDate = hireDate.AddDays(15);
-Console.WriteLine(startDate);
-
-int numberOfHoursWorkd = 165;
-
-double d = 1252251511.0;
-int x = (int)d;
+//Console.WriteLine(wholeweight);
 
 
-//Booelan Types wuth relational and logical operator 
+//wage += 3;
+//Console.WriteLine(wage);
+
+//if (wage > 5225) ;
+//Console.WriteLine("Top Paid Emplloyee");
+
+//int numberOfEmployee = 15;
+//numberOfEmployee--;
+
+//DateTime hireDate = new DateTime(2022,3,28,14,30,0);
+//Console.WriteLine(hireDate);
+
+//DateTime startDate = hireDate.AddDays(15);
+//Console.WriteLine(startDate);
+
+//int numberOfHoursWorkd = 165;
+
+//double d = 1252251511.0;
+//int x = (int)d;
+
+
+////Booelan Types wuth relational and logical operator 
 
 
 
@@ -36,19 +36,60 @@ int x = (int)d;
 //Console.WriteLine("i need a answer "+a);
 
 
-Console.WriteLine("Enter the age of the new candidate");
-int agee = int.Parse(Console.ReadLine());
+//Console.WriteLine("Enter the age of the new candidate");
+//int agee = int.Parse(Console.ReadLine());
 
-if (agee < 18)
+
+
+//switch (agee)
+//{
+//    case < 18:
+//        console.writeline("hi hi all");
+//        break;
+//    case > 65:
+//        console.writeline("hi hi all wer rwe rr");
+//        break;
+//    case 23:
+//        console.writeline("greate ,you can start with the application");
+//        break;
+//    default:
+//        console.writeline("hi hi all wer rwrwr hi hhi hi we rsfsd bye bye allr");
+//        break;
+
+
+
+//}
+
+//if (agee < 18)
+//{
+//  Console.WriteLine("HI hi all");
+//}
+//else if(agee > 65)
+//{
+//  Console.WriteLine("HI hi all wer rwe rr");
+// Console.WriteLine("HI hi all wer rwrwr hi hhi hi we rr");
+//}
+//else { 
+//  Console.WriteLine("Greate ,You can start with the application");
+//}
+
+//While Lop uses here
+
+//Console.WriteLine("Enter the age of the new candidate");
+//int max = int.Parse(Console.ReadLine());
+
+//int i = 100;
+
+//while(i > 0)
+//{
+//    Console.WriteLine(i);
+//    i--;
+
+//}
+
+//Console.WriteLine("Loop is over");
+
+while(true)
 {
-    Console.WriteLine("HI hi all");
+    Console.WriteLine(DateTime.Now);
 }
-else if(agee > 65)
-{
-    Console.WriteLine("HI hi all wer rwe rr");
-    Console.WriteLine("HI hi all wer rwrwr hi hhi hi we rr");
-}
-else { 
-    Console.WriteLine("Greate ,You can start with the application");
-}
-   
